b072272744a79d12f81725777f7b05510902a1a1
Daddyjohn63 <john@webandprosper.co.uk>
2023-11-13 11:18:31 +0000

    before file move from dropbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
+// import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  HomeLayout,
+  Landing,
+  Login,
+  Orders,
+  Products,
+  Register,
+  SingleProduct
+} from './pages';
+
 const App = () => {
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 };
---
+++ src/pages/About.jsx
@@ -0,0 +1,4 @@
+function About() {
+  return <h1 className="text-4xl">About</h1>;
+}
+export default About;
